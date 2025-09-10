d0198513f3deb7550e053f5cbc0a03e08e77bf8f
Attila Hogyai <attila.hogyai@gmail.com>
2025-09-10 11:59:58 +0200

    remove not used

--- backend/redmine/members_ddl.sql
+++
@@ -1,8 +0,0 @@
-public.members (
-    id serial4 NOT NULL,
-    user_id int4 DEFAULT 0 NOT NULL,
-    project_id int4 DEFAULT 0 NOT NULL,
-    created_on timestamp NULL,
-    mail_notification bool DEFAULT false NOT NULL,
-    CONSTRAINT members_pkey PRIMARY KEY (id)
-);
