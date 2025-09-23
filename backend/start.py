90c088065bd4d34c95a5c025f96fb87cccfa0da6
Attila Hogyai <attila.hogyai@gmail.com>
2025-09-23 14:16:10 +0200

    test and fixes

--- backend/start.py
+++ backend/start.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    uvicorn.run("api.api:app", host="127.0.0.1", port=8000, reload=True)
+
+    uvicorn.run("api.api:app", host="127.0.0.1", port=8000, reload=True, reload_excludes=["log/**"])
